50e2755cbf2a22d2366c54f2e9a34e0d660c8cf8
DanielCellbrot <33344043+DanielCellbrot@users.noreply.github.com>
2019-05-17 12:03:20 +0200

    Update image-postprocessing-opencv.cpp
    Threading + optimization

--- kiwidetect/src/image-postprocessing-opencv.cpp
+++ kiwidetect/src/image-postprocessing-opencv.cpp
@@ -28,11 +28,14 @@
 #include <iostream>
 #include <memory>
 #include <mutex>
+#include <string>
+
 
 
 using namespace cv;
 using namespace dnn;
 using namespace std;
+using namespace cluon;
 
 float confThreshold = 0.5;
 float nmsThreshold = 0.4;
@@ -40,11 +43,14 @@ int inpWidth = 416;
 int inpHeight = 416;
 vector<string> classes;
 
+// Give the configuration and weight files for the model
+String modelConfiguration = "/opt/sources/src/darknet-yolov3.cfg";
+String modelWeights = "/opt/sources/src/darknet-yolov3_final.weights";
+
 void postprocess(Mat& frame, const vector<Mat>& out);
 
-void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame);
-
 vector<String> getOutputsNames(const Net& net);
+void loop(string NAME, uint32_t HEIGHT, uint32_t WIDTH);
 
 int32_t main(int32_t argc, char **argv) {
     int32_t retCode{1};
@@ -66,176 +72,126 @@ int32_t main(int32_t argc, char **argv) {
         const std::string NAME{commandlineArguments["name"]};
         const uint32_t WIDTH{static_cast<uint32_t>(std::stoi(commandlineArguments["width"]))};
         const uint32_t HEIGHT{static_cast<uint32_t>(std::stoi(commandlineArguments["height"]))};
-        const bool VERBOSE{commandlineArguments.count("verbose") != 0};
 
-        // Attach to the shared memory.
-        std::unique_ptr<cluon::SharedMemory> sharedMemory{new cluon::SharedMemory{NAME}};
-        if (sharedMemory && sharedMemory->valid()) {
-            std::clog << argv[0] << ": Attached to shared memory '" << sharedMemory->name() << " (" << sharedMemory->size() << " bytes)." << std::endl;
 
             // Interface to a running OpenDaVINCI session; here, you can send and receive messages.
-            cluon::OD4Session od4{static_cast<uint16_t>(std::stoi(commandlineArguments["cid"]))};
-
-            // Load names of classes
-            string classesFile = "/opt/sources/src/classes.names";
-            ifstream ifs(classesFile.c_str());
-            string line;
-            while (getline(ifs, line)) classes.push_back(line);
-            
-            // Give the configuration and weight files for the model
-            String modelConfiguration = "/opt/sources/src/darknet-yolov3.cfg";
-            String modelWeights = "/opt/sources/src/darknet-yolov3_final.weights";
-
-            // Load the network
-            Net net = readNetFromDarknet(modelConfiguration, modelWeights);
-            net.setPreferableBackend(DNN_BACKEND_OPENCV);
-            net.setPreferableTarget(DNN_TARGET_CPU);
-
-            // Open a video file or an image file or a camera stream.
-            string str, outputFile;
-            VideoCapture cap;
-            VideoWriter video;
-            Mat frame, blob;
+            //cluon::OD4Session od4{static_cast<uint16_t>(std::stoi(commandlineArguments["cid"]))};
+                // Load the network
 
 
-            // Endless loop; end the program by pressing Ctrl-C.
-            while (od4.isRunning()) {
-
-
-                
-                // Wait for a notification of a new frame.
-                sharedMemory->wait();
-                
-
-                // Lock the shared memory.
-                sharedMemory->lock();
-                {
-                    // Copy image into cvMat structure.
-                    // Be aware of that any code between lock/unlock is blocking
-                    // the camera to provide the next frame. Thus, any
-                    // computationally heavy algorithms should be placed outside
-                    // lock/unlock.
-                    Mat wrapped(HEIGHT, WIDTH, CV_8UC4, sharedMemory->data());
-                    frame = wrapped.clone();
-                    //blob = wrapped.clone();
-                    
-                    //blob.convertTo(blob, CV_8UC3);
-                    
-                }
-                sharedMemory->unlock();
-                cvtColor(frame, frame, CV_BGRA2BGR);
-                
-
-                // Create a 4D blob from a frame.
-                blobFromImage(frame, blob, 1/255.0, cvSize(inpWidth, inpHeight), Scalar(0,0,0), true, false);
-                
-
-                //Sets the input to the network
-                net.setInput(blob);
-                
-                // Runs the forward pass to get output of the output layers
-                vector<Mat> outs;
-                net.forward(outs, getOutputsNames(net));
-                
-                // Remove the bounding boxes with low confidence
-                postprocess(frame, outs);
-                
-                // Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
-                vector<double> layersTimes;
-                double freq = getTickFrequency() / 1000;
-                double t = net.getPerfProfile(layersTimes) / freq;
-                string label = format("delay: %.2f ms", t);
-                putText(frame, label, Point(0, 15), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255));
-                
-                // Write the frame with the detection boxes
-                Mat detectedFrame;
-                frame.convertTo(detectedFrame, CV_8U);
-                //video.write(detectedFrame);
-
-                // Display image.
-                if (VERBOSE) {
-                    imshow(sharedMemory->name().c_str(), frame);
-                    waitKey(1);
-                }
-            }
-        }
+            cout << "staritng thread 1 ...\n";
+            thread t1(loop, NAME, HEIGHT, WIDTH);
+            cout << "staritng thread 2 ...\n";
+            thread t2(loop, NAME, HEIGHT, WIDTH);
+            cout << "staritng thread 3 ...\n";
+            thread t3(loop, NAME, HEIGHT, WIDTH);
+            t1.join();
+            t2.join();
+            t3.join();
         retCode = 0;
     }
     return retCode;
 }
 
+
+void loop(string NAME, uint32_t HEIGHT, uint32_t WIDTH){
+    //cout << "New thread started\n";
+
+
+    Net net = readNetFromDarknet(modelConfiguration, modelWeights);
+    net.setPreferableBackend(DNN_BACKEND_OPENCV);
+    net.setPreferableTarget(DNN_TARGET_CPU);
+    //cout << "Loaded network\n";
+    // Attach to the shared memory.
+    std::unique_ptr<cluon::SharedMemory> sharedMemory{new cluon::SharedMemory{NAME}};
+    if (sharedMemory && sharedMemory->valid()) {
+        std::clog  << ": Attached to shared memory '" << sharedMemory->name() << " (" << sharedMemory->size() << " bytes)." << std::endl;
+    }
+    //cout << "Attached to shared memory done\n";
+    Mat temp, frame, blob;
+    // Endless loop; end the program by pressing Ctrl-C.
+    while (1) {
+        sharedMemory->wait();
+        sharedMemory->lock();
+        {
+            Mat wrapped(HEIGHT, WIDTH, CV_8UC4, sharedMemory->data());
+            temp = wrapped.clone();
+        }
+        frame = temp(Rect(0, 120, WIDTH, 240)).clone();
+        sharedMemory->unlock();
+        cvtColor(frame, frame, CV_BGRA2BGR);
+        blobFromImage(frame, blob, 1/255.0, cvSize(inpWidth, inpHeight), Scalar(0,0,0), true, false);
+        net.setInput(blob);
+        vector<Mat> outs;
+        net.forward(outs, getOutputsNames(net));
+        postprocess(frame, outs);
+        //cout << "postprocess done\n";
+    }
+
+
+}
+
+
 // Remove the bounding boxes with low confidence using non-maxima suppression
 void postprocess(Mat& frame, const vector<Mat>& outs)
 {
+    //cout << "postprocessing initiated\n";
     vector<int> classIds;
     vector<float> confidences;
     vector<Rect> boxes;
-    
+
     for (size_t i = 0; i < outs.size(); ++i)
     {
-        // Scan through all the bounding boxes output from the network and keep only the
-        // ones with high confidence scores. Assign the box's class label as the class
-        // with the highest score for the box.
+
         float* data = (float*)outs[i].data;
+        int centerX = 0;
+        int centerY = 0;
+        int width = 0;
+        int height = 0;
+        int left = 0;
+        int top = 0;
         for (int j = 0; j < outs[i].rows; ++j, data += outs[i].cols)
         {
             Mat scores = outs[i].row(j).colRange(5, outs[i].cols);
             Point classIdPoint;
             double confidence;
-            // Get the value and location of the maximum score
             minMaxLoc(scores, 0, &confidence, 0, &classIdPoint);
             if (confidence > confThreshold)
             {
-                int centerX = (int)(data[0] * frame.cols);
-                int centerY = (int)(data[1] * frame.rows);
-                int width = (int)(data[2] * frame.cols);
-                int height = (int)(data[3] * frame.rows);
-                int left = centerX - width / 2;
-                int top = centerY - height / 2;
-                
+                centerX = (int)(data[0] * frame.cols);
+                centerY = (int)(data[1] * frame.rows);
+                width = (int)(data[2] * frame.cols);
+                height = (int)(data[3] * frame.rows);
+                left = centerX - width / 2;
+                top = centerY - height / 2;
+
                 classIds.push_back(classIdPoint.x);
                 confidences.push_back((float)confidence);
                 boxes.emplace_back(Rect(left, top, width, height));
             }
         }
     }
-    
-    // Perform non maximum suppression to eliminate redundant overlapping boxes with
-    // lower confidences
+
+
     vector<int> indices;
     NMSBoxes(boxes, confidences, confThreshold, nmsThreshold, indices);
+    OD4Session od4(112);
     for (size_t i = 0; i < indices.size(); ++i)
     {
         int idx = indices[i];
         Rect box = boxes[idx];
-        drawPred(classIds[idx], confidences[idx], box.x, box.y,
-                 box.x + box.width, box.y + box.height, frame);
+        //int width = box.width;
+        int height =  box.height;
+    opendlv::proxy::carReading object;
+    object.Xpos(box.x);
+    object.Ypos(box.y);
+    object.height(height);
+    object.objID("kiwicar");
+    od4.send(object);
+    cout << "Car found, Message sent\n" << box.x << "\n" << box.y << "\n" << height << "\n";
     }
 }
 
-// Draw the predicted bounding box
-void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame)
-{
-    //Draw a rectangle displaying the bounding box
-    rectangle(frame, Point(left, top), Point(right, bottom), Scalar(255, 178, 50), 3);
-    
-    //Get the label for the class name and its confidence
-    string label = format("%.2f", conf);
-    if (!classes.empty())
-    {
-        CV_Assert(classId < (int)classes.size());
-        label = classes[classId] + ":" + label;
-    }
-    
-    //Display the label at the top of the bounding box
-    int baseLine;
-    Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
-    top = max(top, labelSize.height);
-    rectangle(frame, Point(left, top - round(1.5*labelSize.height)), Point(left + round(1.5*labelSize.width), top + baseLine), Scalar(255, 255, 255), FILLED);
-    putText(frame, label, Point(left, top), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,0),1);
-}
-
-// Get the names of the output layers
 vector<String> getOutputsNames(const Net& net)
 {
     static vector<String> names;
@@ -243,16 +199,14 @@ vector<String> getOutputsNames(const Net& net)
     {
         //Get the indices of the output layers, i.e. the layers with unconnected outputs
         vector<int> outLayers = net.getUnconnectedOutLayers();
-        
+
         //get the names of all the layers in the network
         vector<String> layersNames = net.getLayerNames();
-        
+
         // Get the names of the output layers in names
         names.resize(outLayers.size());
         for (size_t i = 0; i < outLayers.size(); ++i)
-        names[i] = layersNames[outLayers[i] - 1];
+            names[i] = layersNames[outLayers[i] - 1];
     }
     return names;
 }
-
-
